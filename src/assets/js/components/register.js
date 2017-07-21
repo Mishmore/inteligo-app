03b9c8d3cd2993b56e17975d297264a13a51a1a9
maryleo.3007 <maryleo.3007@gmail.com>
2017-07-21 17:26:24 -0500

    try de registro

--- src/assets/js/components/register.js
+++ src/assets/js/components/register.js
@@ -1,6 +1,15 @@
 function Register(update) {
 	var container = $('<div class="container-fluid question"></div>');
-  
+
+
 
 	return container;
 }
+
+<div class="container">
+  <input type="email" name="" value="" id="txtEmail" placeholder="email">
+  <input type="password" name="" value="" id="txtPassword">
+  <button type="button" name="button" id="btnLogin">login</button>
+  <button type="button" name="button" id="btnSignup">registrarse</button>
+  <button type="button" name="button" id="btnLogout">logout</button>
+</div>
