ef0f21a37605b18e73fe79644d690576b2f7fc6d
maryleo.3007 <maryleo.3007@gmail.com>
2017-07-21 08:36:58 -0500

    registro y envio de mail ok

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -8076,6 +8076,27 @@ var getJSON = (url, cb) => {
   xhr.send();
 };
 
+//send mail
+var myform = $("form#myform");
+myform.submit(function(event){
+    event.preventDefault();
+
+ // Change to your service ID, or keep using the default service
+ var service_id = "gmail";
+ var template_id = "template_LsICO6Fp";
+
+ myform.find("button").text("Sending...");
+ emailjs.sendForm(service_id,template_id,"myform")
+     .then(function(){
+       alert("Sent!");
+      myform.find("button").text("Send");
+   }, function(err) {
+      alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+      myform.find("button").text("Send");
+   });
+ return false;
+});
+
 var pregunta1 = $('.pregunta1');
 var pregunta2 = $('.pregunta2');
 var pregunta3 = $('.pregunta3');
