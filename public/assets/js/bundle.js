d283880c29f3331c63bd39a7e11ca5dac1f85600
flordeciruela <wendy.reyessan@gmail.com>
2017-08-14 20:45:56 -0500

    gif

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -25727,8 +25727,8 @@ function InicioForm(update) {
 	return container;
 }
 
-function Loading() {
-  var container = $('<div class="container-fluid question"></div>');
+function Loading(update) {
+  var container = $('<div class="container-fluid question" id=loading></div>');
   var divAzul = $('<div class="helper hidden-xs"></div>');
   var logoDiv = $('<div class="logo-div"></div>');
   var img = $('<img class="" src="assets/img/I Complementarias Fondo azul.jpg" alt="logo Inteligo">');
@@ -25737,8 +25737,8 @@ function Loading() {
   var divLoad = $('<div class="loading-div"></div>');
   var loading = $('<div class="progress "></div>');
   var loadingBase = $('<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:100%"><span class="sr-only">100% Complete</span></div>');
-  var containerGif = $('<div class="center-block"></div>');
-  var loadingGif = $('<img src="assets/img/loading.gif">');
+  var containerGif = $('<div class="container-gif text-center"></div>');
+  var loadingGif = $('<img src="assets/img/loading.gif" id="gif">');
 
   container.append(divAzul);
   divAzul.append(logoDiv);
@@ -25748,6 +25748,12 @@ function Loading() {
   loading.append(loadingBase);
   logoDiv.append(img,h2);
   containerGif.append(loadingGif);
+
+  setTimeout(function(){
+    state.screenView = "profile";
+    update();
+  }, 2000);
+
   return container;
 }
 
--- src/assets/js/components/loading.js
+++ src/assets/js/components/loading.js
@@ -1,5 +1,5 @@
-function Loading() {
-  var container = $('<div class="container-fluid question"></div>');
+function Loading(update) {
+  var container = $('<div class="container-fluid question" id=loading></div>');
   var divAzul = $('<div class="helper hidden-xs"></div>');
   var logoDiv = $('<div class="logo-div"></div>');
   var img = $('<img class="" src="assets/img/I Complementarias Fondo azul.jpg" alt="logo Inteligo">');
@@ -8,8 +8,8 @@ function Loading() {
   var divLoad = $('<div class="loading-div"></div>');
   var loading = $('<div class="progress "></div>');
   var loadingBase = $('<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:100%"><span class="sr-only">100% Complete</span></div>');
-  var containerGif = $('<div class="center-block"></div>');
-  var loadingGif = $('<img src="assets/img/loading.gif">');
+  var containerGif = $('<div class="container-gif text-center"></div>');
+  var loadingGif = $('<img src="assets/img/loading.gif" id="gif">');
 
   container.append(divAzul);
   divAzul.append(logoDiv);
@@ -19,5 +19,11 @@ function Loading() {
   loading.append(loadingBase);
   logoDiv.append(img,h2);
   containerGif.append(loadingGif);
+
+  setTimeout(function(){
+    state.screenView = "profile";
+    update();
+  }, 2000);
+
   return container;
 }
