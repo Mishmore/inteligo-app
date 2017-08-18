0ca98dd21cc8a80199c64e02befb9f316f76cec8
maryleo3007 <maryleo.3007@gmail.com>
2017-08-18 18:18:43 -0500

    add chart

--- src/assets/js/components/finalPerfil.js
+++ src/assets/js/components/finalPerfil.js
@@ -1,32 +1,43 @@
 function Profile(update) {
   prueba();
   switchPerfil();
-  var container = $('<div class="container"></div>');
+  var container = $('<div class="container-fluid question"></div>');
+  var divAzul = $('<div class="helper hidden-xs "></div>');
+  var logoDiv = $('<div class="logo-div"></div>');
+  var img = $('<img class="" src="assets/img/I Complementarias Fondo azul.jpg" alt="logo Inteligo">');
+  var h2 = $('<h2 class=""> Conociendo su perfil</h2>');
+
+  var divLoad = $('<div class="loading-div"></div>');
+  var loading = $('<div class="progress "></div>');
+  var loadingBase = $('<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:0%"><span class="sr-only">80% Complete</span></div>');
+
   var div1 = $('<div class="profile"></div>');
   var h1= $('<h1>Basandonos en la respuesta que usted nos ha especificado, hemos determinado que su perfil es: </h1>')
   var h2= $('<h2>Su perfil es: '+ perfil +' </h2>')
   var h3=$('<h3>Te sugerimos este plan de inversión: </h3>')
-  var div3 = $('<div class="informacion"></div>');
-  var img = $('<img src="assets/img/chartpie.jpg">')
-  var span1=$('<span class="texto"><i></i>  Rentabilidad:4.3%  </span>');
-  var span2=$('<span class="texto"><i></i> Mejor Año:27% </span>');
-  var span3=$('<span class="texto"><i></i>Peor Año:16.09%</span>');
+  var div3 = $('<div class="informacion row"></div>');
+  
+  
   var btnReviewPerfil = $('<button type="button" class="btn btn-primary" id="reviewPerfil">Review perfil</button>');
 
   var btnRegister = $('<button type="button" class="btn btn-primary" id="enviarMail">Generar pdf</button>');
   var btnEnviarDatos = $('<button type="button" class="btn btn-primary" id="saveData">Guardar Datos</button>');
   var btnReviewPerfil = $('<button type="button" class="btn btn-primary">Revisar Perfil</button>');
 
+  container.append(divAzul);
+  divAzul.append(logoDiv);
+
+  divAzul.append(divLoad);
+  divLoad.append(loading);
+  loading.append(loadingBase);
+  logoDiv.append(img,h2);
+  
   container.append(div1);
   container.append(div3);
   div1.append(h1);
   div1.append(h2);
   div1.append(h3);
-  //div1.append(img);
-  div3.append(span1);
-  div3.append(span2);
-  div3.append(span3);
-
+  
   div3.append(btnReviewPerfil);
   div3.append(btnRegister);
   div3.append(btnEnviarDatos);
